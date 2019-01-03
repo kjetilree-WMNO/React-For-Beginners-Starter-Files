10333cb0511ee75ad1d3dae1c46d06bd4e1d0b9d
Kjetil Ree <kjeree@cisco.com>
2019-01-03 17:18:36 +0100

    episode 13

--- catch-of-the-day/src/components/App.js
+++ catch-of-the-day/src/components/App.js
@@ -1,21 +1,32 @@
-import React from 'react';
-import Header from './Header'
-import Order from './Order'
-import Inventory from './Inventory'
+import React from "react";
+import Header from "./Header";
+import Order from "./Order";
+import Inventory from "./Inventory";
 
 class App extends React.Component {
-
-    render() {
-        return (
-            <div className="catch-of-the-day">
-                <div className="menu">
-                    <Header tagline="Freesh Seafood Market" age={99} cool={true} />
-                </div>
-                <Order />
-                <Inventory />
-            </div>
-        )
-    }
+  state = {
+    fishes: {},
+    order: {}
+  };
+  addFish = fish => {
+    console.log("Adding a fish: ", fish);
+    const fishes = { ...this.state.fishes };
+    fishes[`fish${Date.now()}`] = fish;
+    this.setState({
+      fishes
+    });
+  };
+  render() {
+    return (
+      <div className="catch-of-the-day">
+        <div className="menu">
+          <Header tagline="Freesh Seafood Market" age={99} cool={true} />
+        </div>
+        <Order />
+        <Inventory addFish={this.addFish} />
+      </div>
+    );
+  }
 }
 
 export default App;
